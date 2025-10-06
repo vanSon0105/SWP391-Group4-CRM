55077222cd9f4a989b59542fe55a6c651dee71ee
hiuphngok <phieu130425@gmail.com>
2025-10-07 01:42:41 +0700

    Get all categories in CategoryDAO

--- src/main/java/controller/homePage/DevicePageController.java
+++ src/main/java/controller/homePage/DevicePageController.java
@@ -6,28 +6,20 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import dao.DeviceDAO;
 
-/**
- * Servlet implementation class DevicePageController
- */
 @WebServlet("/device-page")
 public class DevicePageController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DevicePageController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+    public static final int DEVICE_PER_PAGE = 12;
+    DeviceDAO deviceDao = new DeviceDAO();
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("view/homepage/devicePage.jsp").forward(request, response);
+
 	}
 
 	/**
--- src/main/webapp/view/homepage/devicePage.jsp
+++ src/main/webapp/view/homepage/devicePage.jsp
@@ -1,12 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ page isELIgnored="false" %>
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>NovaCare Shop - Danh mục sản phẩm</title>
-	<link rel="stylesheet" href="./css/shop.css">
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/assets/css/shop.css">
+
 	
 	<!-- Font Awesome -->
 	<link rel="stylesheet"
